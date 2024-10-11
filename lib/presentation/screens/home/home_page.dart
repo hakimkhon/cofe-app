b7ca6699998b3d30ce4e717304c3e8882dcffb51
Hakimkhon Sharifkhonov <hakimkhon1994@gmail.com>
2024-10-11 18:36:13 +0500

    ok

--- lib/presentation/screens/home/home_page.dart
+++ lib/presentation/screens/home/home_page.dart
@@ -1,13 +1,14 @@
 import 'package:cafe/presentation/screens/home/widgets/header_widget.dart';
 import 'package:cafe/presentation/screens/home/widgets/kfc_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
@@ -16,10 +17,26 @@ class HomePage extends StatelessWidget {
               HeaderWidget(),
               // PageView.builder(),
               KfcWidget(),
+              Wrap(
+                alignment: WrapAlignment.spaceBetween,
+                children: [
+                  for (int i = 0; i < 11; i++)
+                  bolacha(),
+                ],
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  bolacha() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      width: 180,
+      height: 80,
+      color: Colors.green,
     );
   }
 }
